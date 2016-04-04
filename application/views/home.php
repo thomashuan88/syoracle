e48d1c3e20f999a9d66f5a8cb030e9b01ea4acad
thomas huan <thomashuan88@gmail.com>
2016-04-04 15:32:48 +0800

    add front end framework

--- application/views/home.php
+++ application/views/home.php
@@ -2,21 +2,16 @@
 <html>
   <head>
     <title>Aurelia</title>
-    <link rel="stylesheet" href="<?php echo $aurelia_base; ?>jspm_packages/npm/font-awesome@4.5.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="<?php echo $aurelia_base; ?>styles/styles.css">
     <meta name="viewport" content="width=device-width, initial-scale=1">
   </head>
 
-  <body aurelia-app="main">
-    <div class="splash">
-      <div class="message">Aurelia Navigation Skeleton</div>
-      <i class="fa fa-spinner fa-spin"></i>
-    </div>
-
+  <body aurelia-app>
     <script src="<?php echo $aurelia_base; ?>jspm_packages/system.js"></script>
     <script src="<?php echo $aurelia_base; ?>config.js"></script>
     <script>
-      System.import('aurelia-bootstrapper');
+      SystemJS.import('aurelia-bootstrapper');
     </script>
   </body>
 </html>
+
