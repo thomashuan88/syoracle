6be6110c62be3357a308e8be89b73052c52c8029
thomas huan <thomashuan88@gmail.com>
2016-04-13 16:29:48 +0800

    config

--- application/config/config.php
+++ application/config/config.php
@@ -368,7 +368,7 @@ $config['encryption_key'] = '';
 |
 */
 $config['sess_driver'] = 'redis';
-$config['sess_save_path'] = 'tcp://192.168.10.10:6379';
+$config['sess_save_path'] = 'tcp://10.105.1.18:6379';
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_expiration'] = 7200;
 $config['sess_match_ip'] = FALSE;
