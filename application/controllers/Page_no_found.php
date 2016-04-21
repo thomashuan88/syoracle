8e476a3cf0ea2740365c4adcfa1d27be0e7ba354
thomas huan <thomashuan88@gmail.com>
2016-04-21 15:34:15 +0800

    SY881-1037 [Oracle] Login - Member Login module

---
+++ application/controllers/Page_no_found.php
@@ -0,0 +1,15 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+
+class Page_no_found extends CI_Controller
+{
+
+    function __construct() {
+        parent::__construct();
+    }
+
+    public function index() {
+        $this->include_path = base_url('static/gentelella')."/"; // tamplate folder
+        $this->load->view('pagenofound');
+    }
+}
